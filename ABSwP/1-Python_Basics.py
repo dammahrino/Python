55b616261f0adcb64d5380d4beed89be29eb8092
Daniel Cadena <31937742+dammahrino@users.noreply.github.com>
2023-04-17 20:44:52 -0600

    Python basics

---
+++ ABSwP/1-Python_Basics.py
@@ -0,0 +1,24 @@
+# Expressions
+print(2 + 2)
+print(5 - 3)
+print(3 * 7)
+print(22 / 7)
+print(2 + 3 * 6)
+print( (2 + 3) * 6 )
+print( (5 - 1) * ( (7 + 1) / (3 - 1) ) )
+
+# Data Types
+print('## Integers')
+print(-2)
+print(30)
+
+print('## Floating point')
+print(3.141592)
+
+print('## Strings')
+print('Hello, World! I\'m a string!')
+print('### String concatenation')
+print(f'Alex + Bob :: {"Alex" + "Bob"}')
+print('### String replication')
+print(f'Alice * 3 :: {"Alice" * 3}')
+print(f'\'Hello\' + \'!\' * 10 :: {"Hello" + "!" * 10}')
